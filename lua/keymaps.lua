0a484640e4735fcc23119b636b2cbf05a212744c
arauto <arauto@arauto.com>
2026-05-06 19:35:56 -0300

    Teclas de atalho para janelas
    Configurado teclas de atalho para troca de janelas.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,11 +1,24 @@
--- keymaps.lua
---
--- Mapeamentos no modo normal (n)
+---@module 'keymaps'
+
+-- ============================================================================
+-- 1. Mapeamentos no modo normal (n)
+-- ============================================================================
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- Mapeamentos no modo visual (v)
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- Mapeamentos no modo insert (i)
+-- ============================================================================
+-- 2. Mapeamentos no modo visual (v)
+-- ============================================================================
 
--- Mapeamentos no modo terminal (t)
+-- ============================================================================
+-- 3. Mapeamentos no modo insert (i)
+-- ============================================================================
+
+-- ============================================================================
+-- 4. Mapeamentos no modo terminal (t)
+-- ============================================================================
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }) -- Sair do modo terminal
