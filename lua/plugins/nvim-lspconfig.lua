a230213014c2a59bdf2508ed289a59609ec5c6cf
arauto <arauto@arauto.com>
2026-05-06 19:38:32 -0300

    Teclas de atalho para LSP
    Foram configuradas teclas de atalho para formatação e diagnóstico.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,22 @@
+---@module 'lsp'
+---
+--- Configuração modular do lsp usando vim.pack (Neovim 0.12+)
+--- Documentação: :h lsp | https://neovim.io/doc/user/lsp/
+
+-- ============================================================================
+-- 1. Registro do plugin via vim.pack
+-- ============================================================================
 vim.pack.add({ 'https://github.com/neovim/nvim-lspconfig' })
 
+-- ============================================================================
+-- 2. Setup
+-- ============================================================================
 vim.lsp.enable({ 'lua_ls' })
+
+-- ============================================================================
+-- 3. Teclas de atalho LSP
+-- ============================================================================
+vim.keymap.set('n', '<leader>le', vim.diagnostic.open_float, { desc = 'Diagnósticos' })
+vim.keymap.set('n', '<leader>lf', function()
+  vim.lsp.buf.format({ async = true })
+end, { desc = 'Formatar' })
